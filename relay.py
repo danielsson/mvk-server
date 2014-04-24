59ab836efa7c7d4ba1e26aaadf1bf79872eca68a
Jesper Simonsson <simonsson.jesper@gmail.com>
2014-04-24 14:54:50 +0200

    added missing file

---
+++ relay.py
@@ -0,0 +1,44 @@
+
+from flask import jsonify, request, abort
+from flask.blueprints import Blueprint
+
+from database import User
+
+
+class RelayService(object):
+	""" docstring """
+	def __init__(self, db, messager):
+		super(RelayService, self).__init__()
+		self.db = db
+		self.messager = messager
+
+	def sendData(self, target, payload):
+		self.messager.sendData(target, payload)
+
+def relay_blueprint(db, relService, current_user):
+
+	rs = Blueprint("Relay", __name__)
+
+	@rs.route('/api/locate/data')
+	def sendData():
+		print "data sending method"
+		data = request.get_json()
+
+		if data == None or 'target' not in data:
+			abort(400) # Bad request
+
+		target = User.query.get_or_404(data['target'])
+
+		requester = current_user()
+
+		if 'data' not in data:
+			abort(400) # Bad request, missing the data to send.
+		
+		payload = data['data']
+
+		locService.sendData(target, payload)
+
+		return jsonify(status="OK")
+
+	return rs
+
