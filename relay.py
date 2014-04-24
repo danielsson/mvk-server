9d88195fe95f0af114e2ec733502a9a8cc5f3103
Jesper Simonsson <simonsson.jesper@gmail.com>
2014-04-24 18:04:51 +0200

    added some documentation, kind of

--- relay.py
+++ relay.py
@@ -6,16 +6,18 @@ from database import User
 
 
 class RelayService(object):
-	""" docstring """
+	""" Provides methods that sends data to the client(s) """
 	def __init__(self, db, messager):
 		super(RelayService, self).__init__()
 		self.db = db
 		self.messager = messager
 
 	def sendData(self, target, payload, action):
+		""" (Tries) sending the specifed data and action to the specifed client """
 		self.messager.sendData(target, payload)
 
 	def broadCast(self, targets, message):
+		""" Send messages to the specifed targets """
 		self.messager.sendBroadcast(targets, message)
 
 def relay_blueprint(db, relService, current_user):
@@ -31,9 +33,6 @@ def relay_blueprint(db, relService, current_user):
 			abort(400) # Bad request
 
 		target = User.query.get_or_404(data['target'])
-
-
-
 		requester = current_user()
 
 		if 'data' not in data or 'action' not in data:
@@ -42,6 +41,7 @@ def relay_blueprint(db, relService, current_user):
 		payload = data['data']
 		action = data['action']
 
+		# Check for actions that should be allowed to use, such that a location can't be faked.
 		if action in ['BROADCAST', 'FOUND', 'BROADCAST']:
 			print "action not allowed"
 			abort(403) # Request okay, but not allowed action sent.
